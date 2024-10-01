e44a827f9e27588a296a107dac5a86b308ee5f53
Thando <www.thandwem045@gmail.com>
2024-10-01 10:01:59 +0200

    Debugged index.js styleActiveBoard

--- index.js
+++ index.js
@@ -112,23 +112,23 @@ function filterAndDisplayTasksByBoard(boardName) {
  console.log(filterAndDisplayTasksByBoard)
 
 
-// function refreshTasksUI() {
-//   filterAndDisplayTasksByBoard(activeBoard);
-// }
+function refreshTasksUI() {
+  filterAndDisplayTasksByBoard(activeBoard);
+}
 
-// // Styles the active board by adding an active class
-// // TASK: Fix Bugs
-// function styleActiveBoard(boardName) {
-//   document.querySelectorAll('.board-btn').foreach(btn => { 
+// Styles the active board by adding an active class
+// TASK: Fix Bugs
+function styleActiveBoard(boardName) {
+  document.querySelectorAll('.board-btn').forEach(btn => { 
     
-//     if(btn.textContent === boardName) {
-//       btn.add('active') 
-//     }
-//     else {
-//       btn.remove('active'); 
-//     }
-//   });
-// }
+    if(btn.textContent === boardName) {
+      btn.classList.add('active') 
+    }
+    else {
+      btn.classList.remove('active'); 
+    }
+  });
+}
 
 
 // function addTaskToUI(task) {
